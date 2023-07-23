7d20fb1ce6f0bfcd348f4e29ae4d4f07495261cb
May <aira.1996@yandex.ru>
2023-07-23 12:27:19 +0300

    fix db

--- src/main/kotlin/main/ManagerBeans.kt
+++ src/main/kotlin/main/ManagerBeans.kt
@@ -11,7 +11,7 @@ class ManagerBeans {
 
     @Bean
     fun createDataSource(): javax.sql.DataSource {
-        val url = "jdbc:postgresql://db:5432/db?user=postgres&password=postgres"
+        val url = "jdbc:postgresql://db:5432/db?user=manager&password=fx9@CyVXH1"
         val dataSource = PGSimpleDataSource()
         dataSource.setUrl(url)
         return dataSource
